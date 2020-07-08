2706eeb9391860c4dfa35c2707d27ea337eb1c59
Regan Loper <reganloper@Regans-MBP.attlocal.net>
2020-07-08 11:01:34 -0500

    added styling to remove input border

--- src/client/components/appstack/Scorecard.tsx
+++ src/client/components/appstack/Scorecard.tsx
@@ -194,48 +194,6 @@ const Scorecard: React.FC<ScorecardProps> = () => {
                                     </tr>
                                 </tbody>
                             </table>
-                            {/* <table className="table table-bordered d-none">
-                            <thead>
-                                <tr>
-                                    <th scope="row">Hole</th>
-                                    <th scope="col">10</th>
-                                    <th scope="col">11</th>
-                                    <th scope="col">12</th>
-                                    <th scope="col">13</th>
-                                    <th scope="col">14</th>
-                                    <th scope="col">15</th>
-                                    <th scope="col">16</th>
-                                    <th scope="col">17</th>
-                                    <th scope="col">18</th>
-                                    <th scope="col">In</th>
-                                    <th scope="col">Total</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <th scope="row">Par</th>
-                                    {holes.slice(9).map((hole, index) => (
-                                        <td key={`back-${index}`} className="align-middle text-center">{hole}</td>
-                                    ))}
-                                    <td className="align-middle text-center">{backNinePar}</td>
-                                    <td className="align-middle text-center">{frontNinePar + backNinePar}</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row" className="align-middle">Regan</th>
-                                    <td className="align-middle text-center"><input type="number" ref={inputRefs[9]} onChange={(e) => updateScorecard(e, 9)} /></td>
-                                    <td className="align-middle text-center"><input type="number" ref={inputRefs[10]} onChange={(e) => updateScorecard(e, 10)} /></td>
-                                    <td className="align-middle text-center"><input type="number" ref={inputRefs[11]} onChange={(e) => updateScorecard(e, 11)} /></td>
-                                    <td className="align-middle text-center"><input type="number" ref={inputRefs[12]} onChange={(e) => updateScorecard(e, 12)} /></td>
-                                    <td className="align-middle text-center"><input type="number" ref={inputRefs[13]} onChange={(e) => updateScorecard(e, 13)} /></td>
-                                    <td className="align-middle text-center"><input type="number" ref={inputRefs[14]} onChange={(e) => updateScorecard(e, 14)} /></td>
-                                    <td className="align-middle text-center"><input type="number" ref={inputRefs[15]} onChange={(e) => updateScorecard(e, 15)} /></td>
-                                    <td className="align-middle text-center"><input type="number" ref={inputRefs[16]} onChange={(e) => updateScorecard(e, 16)} /></td>
-                                    <td className="align-middle text-center"><input type="number" ref={inputRefs[17]} onChange={(e) => updateScorecard(e, 17)} /></td>
-                                    <td className="align-middle text-center">{p1BackNineTotal}</td>
-                                    <td className="align-middle text-center">{score}</td>
-                                </tr>
-                            </tbody>
-                        </table> */}
                         </form>
                     </div>
 
